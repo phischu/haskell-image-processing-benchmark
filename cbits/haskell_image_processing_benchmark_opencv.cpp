6d69e07de386abd3fcd75a17fc6ceef9b72ed27d
phischu <pschuster@uni-koblenz.de>
2014-10-08 00:25:39 +0200

    linking cpp

--- cbits/haskell_image_processing_benchmark_opencv.cpp
+++ cbits/haskell_image_processing_benchmark_opencv.cpp
@@ -1,5 +1,9 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
-extern "C" int foo(){
-    return 5;
+using namespace cv;
+
+extern "C" cv::Mat foo(){
+    return imread( "../koblenz.png", 0 );
 }
 
