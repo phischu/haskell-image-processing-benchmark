8015d2289a6c0a782fe719a7d0b76feafc6de370
phischu <pschuster@uni-koblenz.de>
2014-10-08 03:34:23 +0200

    type signature for mean stencil

--- src/HaskellImageProcessingBenchmark/Yarr.hs
+++ src/HaskellImageProcessingBenchmark/Yarr.hs
@@ -10,7 +10,8 @@ import Data.Word (Word8)
 import Data.Yarr.Eval (dComputeP)
 import Data.Yarr.Flow (dmap)
 
-import Data.Yarr.Convolution (dConvolveLinearDim2WithStaticStencil,dim2St)
+import Data.Yarr.Convolution (dConvolveLinearDim2WithStaticStencil,dim2St,Dim2Stencil)
+import Data.Yarr.Utils.FixedVector (N5)
 
 type Image = UArray D L Dim2 Word8
 
@@ -30,6 +31,7 @@ mean image = do
     convolvedImage <- dComputeP (dConvolveLinearDim2WithStaticStencil meanStencil image)
     return (delay (convolvedImage :: UArray F L Dim2 Word8))
 
+meanStencil :: Dim2Stencil N5 N5 Word8 (Word8 -> Word8 -> IO Word8) Word8
 meanStencil = [dim2St| 1 1 1 1 1
                        1 1 1 1 1
                        1 1 1 1 1
