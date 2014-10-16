4afc000121940d90dfe204094a37117f3bbcacba
phischu <pschuster@uni-koblenz.de>
2014-10-16 12:25:35 +0200

    tweak yarr

--- src/HaskellImageProcessingBenchmark/Yarr.hs
+++ src/HaskellImageProcessingBenchmark/Yarr.hs
@@ -10,7 +10,7 @@ import Data.Word (Word8)
 import Data.Yarr.Eval (dComputeP)
 import Data.Yarr.Flow (dmap)
 
-import Data.Yarr.Convolution (dConvolveLinearDim2WithStaticStencil,dim2St,Dim2Stencil)
+import Data.Yarr.Convolution (dConvolveLinearDim2WithStaticStencil,dim2St,Dim2Stencil,CV,CVL)
 import Data.Yarr.Utils.FixedVector (N1,N5)
 
 type Image = UArray D L Dim2 Word8
@@ -32,14 +32,21 @@ threshold = dmap (\value -> if value > 127 then 255 else 0)
 mean :: Image -> IO Image
 mean image = do
 
-    rowConvolvedImage <- dComputeP (
-        dConvolveLinearDim2WithStaticStencil
-            rowMeanStencil (dmap fromIntegral image))
+    let rowConvolvedImage = dConvolveLinearDim2WithStaticStencil
+            rowMeanStencil (dmap fromIntegral image)
+                :: UArray CV CVL Dim2 Int
 
-    return (dmap (fromIntegral . (`div` 25)) (
-        dConvolveLinearDim2WithStaticStencil (
-            colMeanStencil (
-                rowConvolvedImage :: UArray F L Dim2 Int))))
+    rowConvolvedComputedImage <- dComputeP rowConvolvedImage
+        :: IO (UArray F L Dim2 Int)
+
+    let convolvedImage = dConvolveLinearDim2WithStaticStencil
+            colMeanStencil rowConvolvedComputedImage
+                :: UArray CV CVL Dim2 Int
+
+    convolvedComputedImage <- dComputeP (dmap (fromIntegral . (`div` 25)) convolvedImage)
+        :: IO (UArray F L Dim2 Word8)
+
+    return (delay convolvedComputedImage)
 
 {-# INLINE rowMeanStencil #-}
 rowMeanStencil :: Dim2Stencil N5 N1 Int (Int -> Int -> IO Int) Int
